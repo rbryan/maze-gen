2d1bd297947046d697cbd651c2faad10fb880029
Russell Bryan <bryanr@asmsa.org>
2014-01-26 20:56:07 -0600

    time for other things

--- mazeGen.ml
+++ mazeGen.ml
@@ -3,7 +3,23 @@
 open Printf;;
 open Images;;
 
-type image = { w: int; h: int; name: string}
+
+let get_size args =
+        (int_of_string args.(1), int_of_string args.(2))
+;;
+
+
+let () = 
+        printf "Width:\t%d\n" (fst (get_size Sys.argv));
+        printf "Height:\t%d\n" (snd (get_size Sys.argv))
+;;
+
+let w = fst (get_size Sys.argv);;
+let h = snd (get_size Sys.argv);;
+
+let gen_mat w h c
+
+
 
 
 
